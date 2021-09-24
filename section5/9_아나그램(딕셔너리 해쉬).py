12d6c1fae1bc5acb33ecfb507154485210c78072
June0619 <jjw5050@hotmail.com>
2021-09-24 23:37:44 +0900

    2021-09-24 section5 9강. 아나그램(딕셔너리 해쉬) 추가

--- section5/9_아나그램(딕셔너리 해쉬).py
+++ section5/9_아나그램(딕셔너리 해쉬).py
@@ -12,19 +12,24 @@ word1_dict = dict()
 word2_dict = dict()
 
 for w in word1:
-    if w not in word1_dict.keys():
-        word1_dict[w] = 1
-    else:
-        word1_dict[w] += 1
+    # dictionary 자료형에서 해당 key 에 해당하는 공간이 null 일 경우 기본 값 지정
+    word1_dict[w] = word1_dict.get(w, 0) + 1
+    # if w not in word1_dict.keys():
+    #     word1_dict[w] = 1
+    # else:
+    #     word1_dict[w] += 1
 
 for w in word2:
-    if w not in word2_dict.keys():
-        word2_dict[w] = 1
-    else:
-        word2_dict[w] += 1
+    word2_dict[w] = word2_dict.get(w, 0) + 1
+    # if w not in word2_dict.keys():
+    #     word2_dict[w] = 1
+    # else:
+    #     word2_dict[w] += 1
 
 for key, val in word1_dict.items():
-    if key in word2_dict.keys() and word2_dict[key] == val:
+    # if key in word2_dict.keys() and word2_dict[key] == val:
+    # 위 스타일을 적용하여 더 간결하게 표현
+    if word2_dict.get(key, 0) == val:
         continue
     else:
         print("NO")
