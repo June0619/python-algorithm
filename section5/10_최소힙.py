2c60998008e8ada7258614fcfe485da19ab1101a
June0619 <jjw5050@hotmail.com>
2021-09-27 15:42:12 +0900

    2021-09-27 section5 10강. 최소힙 추가

---
+++ section5/10_최소힙.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import random as r
+import heapq as hq
+
+sys.stdin = open('input.txt', 'rt')
+start_time = time.time()
+
+a = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    if n == 0:
+        if len(a) == 0:
+            print(-1)
+        else:
+            print(hq.heappop(a))
+    else:
+        hq.heappush(a, n)
+
+# 수행시간 체크
+print('수행시간 : ', time.time() - start_time)
