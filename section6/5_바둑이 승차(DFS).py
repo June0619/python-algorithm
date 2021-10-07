5077ce90348b153f38223d8c5b11b7af270bb92d
June0619 <jjw5050@hotmail.com>
2021-10-07 21:47:10 +0900

    2021-10-07 section6 5강. 바둑이 승차(DFS) 수정: cut-edge 로직 추가 (시간복잡도 개선)

--- section6/5_바둑이 승차(DFS).py
+++ section6/5_바둑이 승차(DFS).py
@@ -8,17 +8,24 @@ start_time = time.time()
 max_weight, n = map(int, input().split())
 
 
-def dfs(x, node):
+def dfs(x, summary):
+
+    global answer
+    # cut-edge 1
+    # 합이 이미 최대 무게를 넘은 경우
+    if summary > max_weight:
+        return
+    # cut-edge 2
+    # 이미 더해진 합 + 아직 가지 않은 간선의 모든 합 < 최대 무게인 경우
+    if (summary + sum(input_list[x:n])) < answer:
+        return
     if x == n:
-        if sum(node) > max_weight:
+        if summary > answer:
+            answer = summary
             return
-        else:
-            if sum(node) > max_ch[0]:
-                max_ch[0] = sum(node)
-                return
     else:
-        dfs(x+1, node+[input_list[x]])
-        dfs(x+1, node)
+        dfs(x+1, summary + input_list[x])
+        dfs(x+1, summary)
 
 
 if __name__ == "__main__":
@@ -26,14 +33,12 @@ if __name__ == "__main__":
     for _ in range(n):
         input_list.append(int(input()))
 
-    max_ch = [0]
-    dfs(0, [])
+    total = sum(input_list)
+    answer = 0
+    dfs(0, 0)
 
-    print(max_ch[0])
+    print(answer)
 
 # 수행시간 체크
 print('수행시간 : ', time.time() - start_time)
 
-'''
-단순 dfs 로 구현하니 실행시간 초과됨... 
-'''
